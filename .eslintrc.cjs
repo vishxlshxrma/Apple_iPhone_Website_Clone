c8e21c4826a2e95f45ebbc7a27c75d35115ce4de
Vishal <vishalsharma3657@gmail.comgit>
2024-07-25 20:45:03 +0530

    3D Animated Models Added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,15 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    ':recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.rc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@react-three'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
